971e11d88fa0e03b7761d1354142a4ab97e94d12
MartinRastrilla <rastrillamartin@gmail.com>
2024-08-23 22:39:53 -0300

    MVC created/initialized

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace rivarola_riquelme_rastrilla.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}

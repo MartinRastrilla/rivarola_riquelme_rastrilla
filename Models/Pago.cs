8179c00b277691b12af407577bd2bfa92ff5c173
Ivan <ivan.riquelme135@outlook.com>
2025-01-22 01:37:36 -0300

    Permitir cargar un nuevo pago a ese contrato desde la pantalla del listado

--- Models/Pago.cs
+++ Models/Pago.cs
@@ -1,16 +1,28 @@
 using System.ComponentModel.DataAnnotations.Schema;
+//OBLIGATORIO PARA LAS VALIDACIONES
+using System.ComponentModel.DataAnnotations;
 
 namespace rivarola_riquelme_rastrilla.Models
 {
     public class Pago
     {
-        public required int Id { get; set; }
+        public int Id { get; set; }
 
         [ForeignKey("Contratos")]
         public int? Contrato_id { get; set; }
         public Contratos? Contrato { get; set; }
+
+        [Required(ErrorMessage = "La fecha de pago es obligatoria.")]
+        [DataType(DataType.Date, ErrorMessage = "La fecha de pago debe tener un formato válido.")]
         public DateTime? Fecha_pago { get; set; }
+
+        [Required(ErrorMessage = "El detalle no puede estar vacio.")]
+        [StringLength(255, ErrorMessage = "El detalle no puede tener más de 255 caracteres.")]
         public string? Detalle { get; set; }
+
+        
+        [Required(ErrorMessage = "El importe es obligatorio.")]
+
         public decimal? Importe { get; set; }
     }
 }
